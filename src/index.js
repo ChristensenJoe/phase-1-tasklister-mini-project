9dbb512c7bdda6f490727ac30fcb95d2973af7b9
Joe Christensen <joechristensenwork@gmail.com>
2021-07-02 15:12:50 -0700

    Added TODO List Functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let todoForm = document.querySelector("#create-task-form");
+
+  todoForm.addEventListener("submit", formCallback);
+
+
+  function formCallback(event) {
+    event.preventDefault();
+    let taskList = document.querySelector("#tasks");
+    let task = document.createElement("li");
+    task.textContent = event.target["new-task-description"].value;
+    taskList.prepend(task);
+    event.target["new-task-description"].value = "";
+  }
 });
