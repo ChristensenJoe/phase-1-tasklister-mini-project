60ff63ffd43b2621c5f3d512d2b1561e993781ae
Aaron Dereszynski <aarondski123@gmail.com>
2021-07-02 16:39:38 -0600

    added delete button

--- src/index.js
+++ src/index.js
@@ -9,8 +9,22 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault();
     let taskList = document.querySelector("#tasks");
     let task = document.createElement("li");
-    task.textContent = event.target["new-task-description"].value;
+    let taskDelete = document.createElement('button');
+    
+    task.textContent = event.target["new-task-description"].value+'   ';
+    
+    taskDelete.textContent = 'x';
+    taskDelete.addEventListener('click',deleteButtonCallback);
+
+    task.appendChild(taskDelete);
+    
     taskList.prepend(task);
     event.target["new-task-description"].value = "";
   }
+  function deleteButtonCallback(event){
+    let task = event.target.parentNode;
+    task.remove()
+  }
+
+
 });
